b7fd5c98d69c0fe7e9afbb9af28ec9d21ebee7ad
J.Robles <jaimit1988@gmail.com>
2025-10-09 19:58:31 +0200

    update

--- PROYECTO_INICIAL/src/Main.java
+++ PROYECTO_INICIAL/src/Main.java
@@ -1,11 +1,41 @@
-public class Main {
-          public static void main(String[] args) {
-     
-            System.out.println("Este es la segunda linea del programa");
-            System.out.println("Este es la segunda linea del programa");
-}
-        public void nombreMetodo (){
-}
+// esto es un comentario
+    /*
+    este comentario
+    se puede dividir en
+    varias lineas de codigo
+     */
+// TODO me falta esto
+
+public static void main(String[] args) {
+
+        int edad
+
+
+        System.out.println("Este es la segunda linea del programa");
+        System.out.println("Este es la segunda linea del programa");
+        String miNombre = "Jaime"; // null
+
+
+        char letraDNI = 'A';
+        int edad = 37;
+        double altura = 1.71;
+        float peso = 70.7f;
+        boolean experiencia = false;
+
+
+        System.out.println("Mi nombre es: " + miNombre);
+        System.out.println("La letra de mi dni es: " + letraDNI);
+        System.out.println("Mi edad  es: " + edad);
+        System.out.println("Mi altura es: " + altura);
+        System.out.println("Mi peso es: " + peso);
+        System.out.println("Mi nexperiencia es: " + experiencia);
+        System.out.println();
+    }
+
+    public void nombreMetodo() {
+
+    int edadMetodo = 12;
+    }
 
 
 }
